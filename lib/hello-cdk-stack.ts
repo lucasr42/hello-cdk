33e63ef49d9b4aac690e8660fa7ddf692ed258ec
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-06-22 21:09:42 -0700

    Built the tutorial app

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +13,27 @@ export class HelloCdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        handler: "index.handler",
+        code: lambda.Code.fromInline(`
+            exports.handler = async function(event) {
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify("Hello CDK!"),
+                };
+            };
+        `),
+    });
+
+    const myFunctionUrl = myFunction.addFunctionUrl({
+        authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new cdk.CfnOutput(this, "myFunctionUrlOutput", {
+        value: myFunctionUrl.url,
+    })
+
+
   }
 }
